b2dfd490d13401ed97cbd1fc7acf8c36e12f9399
Akshat Mittal <itsreallyakshat@gmail.com>
2024-06-05 03:51:51 +0530

    Add settler

--- src/AdvancedOrders.sol
+++ src/AdvancedOrders.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
-import {IERC20} from "forge-std/interfaces/IERC20.sol";
-import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
-import {TickMath} from "v4-core/libraries/TickMath.sol";
-import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
-import {BaseHook} from "v4-periphery/BaseHook.sol";
-import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
-import {ERC1155} from "solmate/tokens/ERC1155.sol";
-import {CurrencyLibrary, Currency} from "v4-core/types/Currency.sol";
-import {Hooks} from "v4-core/libraries/Hooks.sol";
-import {StateLibrary} from "v4-core/libraries/StateLibrary.sol";
-import {PoolKey} from "v4-core/types/PoolKey.sol";
+import { IPoolManager } from "v4-core/interfaces/IPoolManager.sol";
+import { IERC20 } from "forge-std/interfaces/IERC20.sol";
+import { FixedPointMathLib } from "solmate/utils/FixedPointMathLib.sol";
+import { TickMath } from "v4-core/libraries/TickMath.sol";
+import { BalanceDelta } from "v4-core/types/BalanceDelta.sol";
+import { BaseHook } from "v4-periphery/BaseHook.sol";
+import { PoolId, PoolIdLibrary } from "v4-core/types/PoolId.sol";
+import { ERC1155 } from "solmate/tokens/ERC1155.sol";
+import { CurrencyLibrary, Currency } from "v4-core/types/Currency.sol";
+import { Hooks } from "v4-core/libraries/Hooks.sol";
+import { StateLibrary } from "v4-core/libraries/StateLibrary.sol";
+import { PoolKey } from "v4-core/types/PoolKey.sol";
 
 /**
  * @title AdvancedOrders
@@ -25,27 +25,41 @@ contract AdvancedOrders is BaseHook, ERC1155 {
     using CurrencyLibrary for Currency;
     using StateLibrary for IPoolManager;
 
-    enum OrderType { STOP_LOSS, BUY_STOP, BUY_LIMIT, TAKE_PROFIT }
-    enum OrderStatus { OPEN, EXECUTED, CANCELED }
-    
+    enum OrderType {
+        STOP_LOSS,
+        BUY_STOP,
+        BUY_LIMIT,
+        TAKE_PROFIT
+    }
+    enum OrderStatus {
+        OPEN,
+        EXECUTED,
+        CANCELED
+    }
+
     struct Order {
         address user;
         OrderType orderType;
         uint256 amountIn;
-        uint256 triggerTick;
-        bool OrderStatus;
+        int24 triggerTick;
+        OrderStatus status;
         bool zeroForOne;
     }
 
     // IPoolManager public pool;
     // EigenLayerAVS public avs; // TODO: integrate avs
     uint256 public orderCount;
+    PoolKey public poolKey;
     mapping(bytes32 => Order) public orders;
     mapping(int24 tick => mapping(bool zeroForOne => Order[])) public orderPositions;
     mapping(PoolId => int24) public tickLowerLasts;
 
-    event OrderPlaced(bytes32 indexed orderId, address indexed user, OrderType orderType, uint256 amountIn, uint256 triggerPrice);
-    event OrderExecuted(bytes32 indexed orderId, address indexed user, OrderType orderType, uint256 amountIn, uint256 triggerPrice);
+    event OrderPlaced(
+        bytes32 indexed orderId, address indexed user, OrderType orderType, uint256 amountIn, int24 triggerPrice
+    );
+    event OrderExecuted(
+        bytes32 indexed orderId, address indexed user, OrderType orderType, uint256 amountIn, int24 triggerPrice
+    );
 
     // -- 1155 state -- //
     mapping(uint256 tokenId => TokenIdData) public tokenIdIndex;
@@ -59,9 +73,7 @@ contract AdvancedOrders is BaseHook, ERC1155 {
         bool zeroForOne;
     }
 
-    constructor(
-        IPoolManager _manager
-    ) BaseHook(_manager) ERC1155() {}
+    constructor(IPoolManager _manager) BaseHook(_manager) ERC1155() { }
 
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
         return Hooks.Permissions({
@@ -87,40 +99,41 @@ contract AdvancedOrders is BaseHook, ERC1155 {
         poolManagerOnly
         returns (bytes4)
     {
+        poolKey = key;
         setTickLowerLast(key.toId(), getTickLower(tick, key.tickSpacing));
+
         return AdvancedOrders.afterInitialize.selector;
     }
 
-    /**
-     * @notice Places an order of a specified type.
-     * @param orderType The type of the order.
-     * @param amountIn The amount of tokens involved in the order.
-     * @param triggerPrice The price at which the order should be triggered.
-     */
-    function placeOrder(OrderType orderType, uint256 amountIn, uint256 triggerPrice, PoolKey calldata poolKey, int24 tickLower) external returns (bytes32 orderId) {
+    function placeOrder(
+        OrderType orderType,
+        uint256 amountIn,
+        int24 _triggerTick,
+        PoolKey calldata _poolKey,
+        int24 tickLower
+    ) external returns (bytes32 orderId) {
         require(amountIn > 0, "Amount must be greater than 0");
-        require(triggerPrice > 0, "Trigger price must be greater than 0");
 
         orderId = keccak256(abi.encodePacked(orderCount, msg.sender, block.timestamp));
         bool zeroForOne = (orderType == OrderType.BUY_STOP) || (orderType == OrderType.STOP_LOSS);
-        orders[orderId] = Order(msg.sender, orderType, amountIn, triggerPrice, false, zeroForOne);
-        int24 tick = getTickLower(tickLower, poolKey.tickSpacing);
+        orders[orderId] = Order(msg.sender, orderType, amountIn, _triggerTick, OrderStatus.OPEN, zeroForOne);
+        int24 tick = getTickLower(tickLower, _poolKey.tickSpacing);
         orderPositions[tick][zeroForOne].push(orders[orderId]);
         orderCount++;
 
         // Mint receipt token
-        uint256 tokenId = getTokenId(poolKey, tick, zeroForOne);
+        uint256 tokenId = getTokenId(_poolKey, tick, zeroForOne);
         if (!tokenIdExists[tokenId]) {
             tokenIdExists[tokenId] = true;
-            tokenIdIndex[tokenId] = TokenIdData({poolKey: poolKey, tickLower: tick, zeroForOne: zeroForOne});
+            tokenIdIndex[tokenId] = TokenIdData({ poolKey: _poolKey, tickLower: tick, zeroForOne: zeroForOne });
         }
         _mint(msg.sender, tokenId, amountIn, "");
         totalSupply[tokenId] += amountIn;
 
         // Transfer token0 to this contract
-        address token = zeroForOne ? Currency.unwrap(poolKey.currency0) : Currency.unwrap(poolKey.currency1);
+        address token = zeroForOne ? Currency.unwrap(_poolKey.currency0) : Currency.unwrap(_poolKey.currency1);
         IERC20(token).transferFrom(msg.sender, address(this), amountIn);
-        emit OrderPlaced(orderId, msg.sender, orderType, amountIn, triggerPrice);
+        emit OrderPlaced(orderId, msg.sender, orderType, amountIn, _triggerTick);
     }
 
     function afterSwap(
@@ -156,20 +169,27 @@ contract AdvancedOrders is BaseHook, ERC1155 {
                 }
             }
         }
-        return (AdvancedOrders.afterSwap.selector, tick); 
+        return (AdvancedOrders.afterSwap.selector, tick);
     }
 
-    // function performSwap(PoolKey memory key, IPoolManager.SwapParams memory params, address receiver)
-    // internal
-    // returns (BalanceDelta delta)
-    // {
-    //     delta = abi.decode(poolManager.lock(abi.encodeCall(this.handleSwap, (key, params, receiver))), (BalanceDelta));
+    function settleOrder(bytes32 orderId, bytes calldata _extraData) external {
+        Order storage order = orders[orderId];
 
-    //     uint256 ethBalance = address(this).balance;
-    //     if (ethBalance > 0) {
-    //         CurrencyLibrary.NATIVE.transfer(msg.sender, ethBalance);
-    //     }
-    // }
+        // TODO: Add check to see if the order can be settled based on current tick and order type
+
+        address tokenIn = order.zeroForOne ? Currency.unwrap(poolKey.currency1) : Currency.unwrap(poolKey.currency0);
+        address tokenOut = order.zeroForOne ? Currency.unwrap(poolKey.currency0) : Currency.unwrap(poolKey.currency1);
+        IERC20(tokenIn).transfer(msg.sender, order.amountIn);
+
+        (bool success,) = msg.sender.call(
+            abi.encodeWithSignature("settleCallback(address,address,bytes)", tokenIn, tokenOut, _extraData)
+        );
+        require(success, "Settle callback failed");
+        // TODO: Validate fullfilment?
+
+        order.status = OrderStatus.EXECUTED;
+        IERC20(tokenOut).transfer(order.user, IERC20(tokenOut).balanceOf(address(this)));
+    }
 
     function getTick(PoolId poolId) private view returns (int24 tick) {
         (, tick,,) = poolManager.getSlot0(poolId);
@@ -177,15 +197,11 @@ contract AdvancedOrders is BaseHook, ERC1155 {
 
     // -- 1155 -- //
     function uri(uint256) public pure override returns (string memory) {
-        return "https://uniswap-advanced-orders.com/";
+        return "";
     }
 
-    function getTokenId(PoolKey calldata poolKey, int24 tickLower, bool zeroForOne)
-        public
-        pure
-        returns (uint256)
-    {
-        return uint256(keccak256(abi.encodePacked(poolKey.toId(), tickLower, zeroForOne)));
+    function getTokenId(PoolKey calldata _poolKey, int24 tickLower, bool zeroForOne) public pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(_poolKey.toId(), tickLower, zeroForOne)));
     }
 
     function redeem(uint256 tokenId, uint256 amountIn, address destination) external {
---
+++ src/OrderSettler.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import { IERC20 } from "forge-std/interfaces/IERC20.sol";
+
+contract OrderSettler {
+    struct Call {
+        address target;
+        uint256 value;
+        bytes callData;
+    }
+
+    function initiateSettle(address _poolHook, uint256 _orderId, Call[] calldata calls) external {
+        (bool success,) =
+            _poolHook.call(abi.encodeWithSignature("settleOrder(bytes32,bytes)", _orderId, abi.encode(calls)));
+        require(success);
+    }
+
+    function settleCallback(address, address tokenOut, bytes calldata _extraData) external {
+        Call[] memory calls = abi.decode(_extraData, (Call[]));
+        uint256 length = calls.length;
+
+        // settle order via call execution
+        for (uint256 i = 0; i < length;) {
+            Call memory calli = calls[i];
+            (bool success,) = calli.target.call(calli.callData);
+
+            // Gas Optimized via Multicall3
+            assembly {
+                // Revert if the call fails and failure is not allowed
+                // `allowFailure := calldataload(add(calli, 0x20))` and `success := mload(result)`
+                if iszero(or(calldataload(add(calli, 0x20)), mload(success))) {
+                    // set "Error(string)" signature: bytes32(bytes4(keccak256("Error(string)")))
+                    mstore(0x00, 0x08c379a000000000000000000000000000000000000000000000000000000000)
+                    // set data offset
+                    mstore(0x04, 0x0000000000000000000000000000000000000000000000000000000000000020)
+                    // set length of revert string
+                    mstore(0x24, 0x0000000000000000000000000000000000000000000000000000000000000017)
+                    // set revert string: bytes32(abi.encodePacked("Multicall3: call failed"))
+                    mstore(0x44, 0x4d756c746963616c6c333a2063616c6c206661696c6564000000000000000000)
+                    revert(0x00, 0x64)
+                }
+            }
+            unchecked {
+                ++i;
+            }
+        }
+
+        // Send tokens back to the hook.
+        IERC20(tokenOut).transfer(msg.sender, IERC20(tokenOut).balanceOf(address(this)));
+    }
+}
